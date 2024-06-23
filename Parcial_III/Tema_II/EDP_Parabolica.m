d09cf840030a158453921d2568e8149539d10651
JoJoWitness <Jordano470@gmail.com>
2024-06-23 11:51:33 -0400

    ?

--- Parcial_III/Tema_II/EDP_Parabolica.m
+++ Parcial_III/Tema_II/EDP_Parabolica.m
@@ -8,18 +8,31 @@ function [x, wt] = FiniteDifference(fx,l,T,alpha,n,m)
     d=1-2*lambda;
 
     A=diag(d*ones(m-1,1));
-    A=A+diag(lambda*ones(m-2,1))+diag(lambda*ones(m-2,-1));
+    A=A+diag(lambda*ones(m-2,1),1)+diag(lambda*ones(m-2,1),-1);
 
     w0=fx(x(n:m));
-    w1=A*w0;
-    w2=A*w1;
+    % w1=A.*w0;
+    % w2=A.*w1;
 
     w(:,1) =w0;
 
-    for j=1:2
+    for j=1:n
         w(:,j+1) = A*w(:,j);
     end
 
     wt=w';
 
 end
+
+fx= @(x) sin(pi*x)+x.*(1-x);
+m=4;
+T= 0.1;
+n =2;
+l=2;
+alpha=1;
+
+
+
+[x, w] = FiniteDifference(fx,l,T,alpha,n,m);
+display(x);
+display(w);

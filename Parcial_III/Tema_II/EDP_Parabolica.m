a6c5530afed8f4c03db8291858e81e3140dae4f5
JoJoWitness <Jordano470@gmail.com>
2024-06-23 13:20:44 -0400

    Fixed?

--- Parcial_III/Tema_II/EDP_Parabolica.m
+++ Parcial_III/Tema_II/EDP_Parabolica.m
@@ -1,4 +1,4 @@
-function [x, wt] = FiniteDifference(fx,l,T,alpha,n,m)
+function [x, wt] = FiniteDifference(fx,F,l,T,alpha,n,m)
 
     h=l/m;
     k= T/n;
@@ -15,9 +15,10 @@ function [x, wt] = FiniteDifference(fx,l,T,alpha,n,m)
     % w2=A.*w1;
 
     w(:,1) =w0;
+    Fx = F(x(2:m)) * k; 
 
     for j=1:n
-        w(:,j+1) = A*w(:,j);
+        w(:,j+1) = A*w(:,j)+Fx(j);
     end
 
     wt=w';
@@ -25,14 +26,15 @@ function [x, wt] = FiniteDifference(fx,l,T,alpha,n,m)
 end
 
 fx= @(x) sin(pi*x)+x.*(1-x);
+F = @(x) 2 + zeros(size(x)); 
 m=4;
-T= 0.1;
+T=0.1;
 n =2;
-l=2;
+l=1;
 alpha=1;
 
 
 
-[x, w] = FiniteDifference(fx,l,T,alpha,n,m);
+[x, w] = FiniteDifference(fx,F,l,T,alpha,n,m);
 display(x);
 display(w);

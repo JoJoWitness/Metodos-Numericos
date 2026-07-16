f627ad1e782496992d31d1346e9ff9c7d97b1a81
JoJoCarnation <98922117+JoJoWitness@users.noreply.github.com>
2026-07-16 01:28:19 -0400

    Jacobi convertido en funcion

--- Parcial_II/Tema_I/F_Jacobi.m
+++ Parcial_II/Tema_I/F_Jacobi.m
@@ -1,37 +1,40 @@
-
-A = [10 -1 0; -1 10 -2; 0 -2 10];
-b = [9;7;6];
-x =[0;0;0];
-tol = 1e-3;
-it_max = 1000;
-
+function [x, k] = Jacobi(A, b, x0, tol, itmax)
   dim = size(A);
   y = zeros(dim(2),1);
   k=1;
-  band = 0;
-  while k <= it_max && ~band
+  while k <= itmax
       for i=1:dim(2)
-          sum=0;
+          s=0;
           for j=1:dim(2)
               if j~=i
-                  sum = sum + A(i,j).*x(j);
+                  s = s + A(i,j).*x0(j);
               end
           end
-          y(i) = (b(i) - sum)./A(i,i);
+          y(i) = (b(i) - s)./A(i,i);
       end
-      if norm(y-x,inf) < tol
-          band = 1;
+      if norm(y-x0,inf) < tol
+          x=y;
+          break;
       end
       k = k + 1;
-      x = y;
+      x0 = y;
   end
-
-  if band == 1
-      fprintf('La solucion es: \n');
-      disp(y);
-      fprintf('El numero de iteraciones es: %d\n', k);
-  else
+  
+  if k > itmax
       fprintf('El metodo no converge\n');
       fprintf('El numero de iteraciones es: %d\n', k);
-
   end
+
+end
+
+A = [10 -1 0; -1 10 -2; 0 -2 10];
+b = [9;7;6];
+x0 =[0;0;0];
+tol = 1e-3;
+itmax = 1000;
+
+[x, k] = Jacobi(A, b, x0, tol, itmax)
+
+fprintf('La solucion es: \n');
+disp(x);
+fprintf('El numero de iteraciones es: %d\n', k);
